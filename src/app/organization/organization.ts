4215c57134267c91a05c0c2cbeb948b4c0d5a7e8
Rodrigo Linares <rodolinares19@gmail.com>
2026-04-23 18:22:54 -0500

    feat: add filtering capabilities to division table columns

--- src/app/organization/organization.ts
+++ src/app/organization/organization.ts
@@ -5,7 +5,13 @@ import { NzIconModule } from 'ng-zorro-antd/icon'
 import { NzInputModule } from 'ng-zorro-antd/input'
 import { NzRadioModule } from 'ng-zorro-antd/radio'
 import { NzSelectModule } from 'ng-zorro-antd/select'
-import { NzTableModule, NzTableSortFn, NzTableSortOrder } from 'ng-zorro-antd/table'
+import {
+  NzTableFilterFn,
+  NzTableFilterList,
+  NzTableModule,
+  NzTableSortFn,
+  NzTableSortOrder
+} from 'ng-zorro-antd/table'
 import { NzTabsModule } from 'ng-zorro-antd/tabs'
 import { NzTypographyModule } from 'ng-zorro-antd/typography'
 import { Subscription } from 'rxjs'
@@ -16,6 +22,9 @@ import { DivisionModel } from '../division/models/division.model'
 interface ColumnItem {
   label: string
   value: string
+  filterFn: NzTableFilterFn<DivisionModel> | null
+  filterMultiple: boolean
+  filterList: NzTableFilterList
   sortOrder: NzTableSortOrder | null
   sortFn: NzTableSortFn<DivisionModel> | null
   sortDirections: NzTableSortOrder[]
@@ -38,53 +47,7 @@ interface ColumnItem {
   styleUrl: './organization.scss'
 })
 export class Organization implements OnInit, OnDestroy {
-  columns: ColumnItem[] = [
-    {
-      label: 'División',
-      value: 'name',
-      sortOrder: null,
-      sortFn: (a: DivisionModel, b: DivisionModel) => a.name.localeCompare(b.name),
-      sortDirections: ['ascend', 'descend', null]
-    },
-    {
-      label: 'División Superior',
-      value: 'parent.name',
-      sortOrder: null,
-      sortFn: (a: DivisionModel, b: DivisionModel) =>
-        (a.parent?.name || '').localeCompare(b.parent?.name || ''),
-      sortDirections: ['ascend', 'descend', null]
-    },
-    {
-      label: 'Colaboradores',
-      value: 'collaboratorCount',
-      sortOrder: null,
-      sortFn: (a: DivisionModel, b: DivisionModel) => a.collaboratorCount - b.collaboratorCount,
-      sortDirections: ['ascend', 'descend', null]
-    },
-    {
-      label: 'Nivel',
-      value: 'level',
-      sortOrder: null,
-      sortFn: (a: DivisionModel, b: DivisionModel) => a.level - b.level,
-      sortDirections: ['ascend', 'descend', null]
-    },
-    {
-      label: 'Subdivisiones',
-      value: 'subdivisions.length',
-      sortOrder: null,
-      sortFn: (a: DivisionModel, b: DivisionModel) => a.subdivisions.length - b.subdivisions.length,
-      sortDirections: ['ascend', 'descend', null]
-    },
-    {
-      label: 'Embajadores',
-      value: 'ambassadorName',
-      sortOrder: null,
-      sortFn: (a: DivisionModel, b: DivisionModel) =>
-        (a.ambassadorName || '').localeCompare(b.ambassadorName || ''),
-      sortDirections: ['ascend', 'descend', null]
-    }
-  ]
-
+  columns: ColumnItem[] = []
   divisionData: DivisionModel[] = []
   divisionsFiltered: DivisionModel[] = []
   loading = true
@@ -95,8 +58,97 @@ export class Organization implements OnInit, OnDestroy {
   private divisionService = inject(Division)
   private subscriptions: Subscription[] = []
 
+  private setupColumns(data: DivisionModel[]) {
+    const columns: ColumnItem[] = [
+      {
+        label: 'División',
+        value: 'name',
+        filterFn: (list: string[], item: DivisionModel) =>
+          list.some(name => item.name.includes(name)),
+        filterMultiple: true,
+        filterList: data.map(div => ({ text: div.name, value: div.name })),
+        sortOrder: null,
+        sortFn: (a: DivisionModel, b: DivisionModel) => a.name.localeCompare(b.name),
+        sortDirections: ['ascend', 'descend', null]
+      },
+      {
+        label: 'División Superior',
+        value: 'parent.name',
+        filterFn: (list: string[], item: DivisionModel) =>
+          list.some(name => item.parent?.name.includes(name)),
+        filterMultiple: true,
+        filterList: Array.from(new Set(data.map(div => div.parent?.name).filter(name => name))).map(
+          name => ({ text: name!, value: name! })
+        ),
+        sortOrder: null,
+        sortFn: (a: DivisionModel, b: DivisionModel) =>
+          (a.parent?.name || '').localeCompare(b.parent?.name || ''),
+        sortDirections: ['ascend', 'descend', null]
+      },
+      {
+        label: 'Colaboradores',
+        value: 'collaboratorCount',
+        filterFn: (list: number[], item: DivisionModel) =>
+          list.some(count => item.collaboratorCount === count),
+        filterMultiple: true,
+        filterList: Array.from(new Set(data.map(div => div.collaboratorCount))).map(count => ({
+          text: count.toString(),
+          value: count
+        })),
+        sortOrder: null,
+        sortFn: (a: DivisionModel, b: DivisionModel) => a.collaboratorCount - b.collaboratorCount,
+        sortDirections: ['ascend', 'descend', null]
+      },
+      {
+        label: 'Nivel',
+        value: 'level',
+        filterFn: (list: number[], item: DivisionModel) => list.some(level => item.level === level),
+        filterMultiple: true,
+        filterList: Array.from(new Set(data.map(div => div.level))).map(level => ({
+          text: level.toString(),
+          value: level
+        })),
+        sortOrder: null,
+        sortFn: (a: DivisionModel, b: DivisionModel) => a.level - b.level,
+        sortDirections: ['ascend', 'descend', null]
+      },
+      {
+        label: 'Subdivisiones',
+        value: 'subdivisions.length',
+        filterFn: (list: number[], item: DivisionModel) =>
+          list.some(count => item.subdivisions.length === count),
+        filterMultiple: true,
+        filterList: Array.from(new Set(data.map(div => div.subdivisions.length))).map(count => ({
+          text: count.toString(),
+          value: count
+        })),
+        sortOrder: null,
+        sortFn: (a: DivisionModel, b: DivisionModel) =>
+          a.subdivisions.length - b.subdivisions.length,
+        sortDirections: ['ascend', 'descend', null]
+      },
+      {
+        label: 'Embajadores',
+        value: 'ambassadorName',
+        filterFn: (list: string[], item: DivisionModel) =>
+          list.some(name => item.ambassadorName?.includes(name)),
+        filterMultiple: true,
+        filterList: Array.from(
+          new Set(data.map(div => div.ambassadorName).filter(name => name))
+        ).map(name => ({ text: name!, value: name! })),
+        sortOrder: null,
+        sortFn: (a: DivisionModel, b: DivisionModel) =>
+          (a.ambassadorName || '').localeCompare(b.ambassadorName || ''),
+        sortDirections: ['ascend', 'descend', null]
+      }
+    ]
+
+    this.columns = [...columns]
+  }
+
   private loadData() {
     const sub = this.divisionService.listDivisions().subscribe(data => {
+      this.setupColumns(data)
       this.divisionData = [...data]
       this.divisionsFiltered = [...data]
       this.loading = false
