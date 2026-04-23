95580dc05010c2695e4269b48dc110288dbb87da
Rodrigo Linares <rodolinares19@gmail.com>
2026-04-23 11:56:27 -0500

    feat: integrate ng-zorro icons and update app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
+import { registerLocaleData } from '@angular/common'
+import { provideHttpClient, withFetch } from '@angular/common/http'
+import en from '@angular/common/locales/en'
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
 import { provideRouter } from '@angular/router'
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n'
 
 import { routes } from './app.routes'
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n'
-import { registerLocaleData } from '@angular/common'
-import en from '@angular/common/locales/en'
 
 registerLocaleData(en)
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideNzI18n(en_US)]
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideNzI18n(en_US)
+  ]
 }
