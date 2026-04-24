2d4bd2dcf8c760abb9fa9a03c47a7d4ee9e76246
Rodrigo Linares <rodolinares19@gmail.com>
2026-04-23 22:12:07 -0500

    chore: add environment files for development and production

--- src/app/division/division.ts
+++ src/app/division/division.ts
@@ -2,11 +2,12 @@ import { HttpClient } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
 
 import { DivisionModel } from './models/division.model'
+import { environment } from '../../environments/environment'
 
 @Injectable({ providedIn: 'root' })
 export class Division {
   private http = inject(HttpClient)
-  private url = 'http://localhost:3000/division'
+  private url = `${environment.apiUrl}/division`
 
   listDivisions() {
     return this.http.get<DivisionModel[]>(this.url)
